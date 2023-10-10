9d01049eb6f08e1a313fb49ca388d68d48175c87
Shafin Ahemd <sh.shafin20@gmail.com>
2023-10-10 23:11:41 +0600

    Accepted Code

--- Work_Smarter_Not_Harder.cpp
+++ Work_Smarter_Not_Harder.cpp
@@ -10,19 +10,12 @@ int main()
         int l , v1 , v2;
         cin >> l >> v1 >> v2;
         int t1 , t2;
-        if(l % v1 != 0)
-            t1 = floor(l / v1) +1;
-        else 
-            t1 = l / v1;    
-        if(l % v2 != 0)
-            t2 = floor(l / v2) +1; 
-        else
-            t2 = l / v2;
-            
+        t1 = ceil(l*1.0 / v1*1.0);
+        t2 = ceil(l*1.0 / v2*1.0);
         if(t1 - t2 - 1 >= 0)    
             cout << t1 - t2 - 1 << endl;
         else
-            cout << -1 << endl;    
+            cout << -1 << endl;
     }
     return 0;
 }
